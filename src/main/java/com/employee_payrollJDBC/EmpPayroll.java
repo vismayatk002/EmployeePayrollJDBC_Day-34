a148f3a8e6751c7893ad6a0a2bc474902c5b6bdb
vismayatk002 <vismayatk002@gmail.com>
2021-08-31 16:29:19 +0530

    Added Welcome message

---
+++ src/main/java/com/employee_payrollJDBC/EmpPayroll.java
@@ -0,0 +1,9 @@
+package com.employee_payrollJDBC;
+
+public class EmpPayroll {
+	
+    public static void main( String[] args ) {
+    	
+        System.out.println( "Welcome to Employee Payroll Service" );
+    }
+}
